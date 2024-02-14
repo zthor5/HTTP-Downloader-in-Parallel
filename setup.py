45aea1eb3fd75d5f63594fce10bfe779bb176940
zthor5 <120127908+zthor5@users.noreply.github.com>
2024-02-14 15:12:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='HttpDownloader',
  version='0.1',
- description='This program does what the name entails.',
+ description='Designed to download a single file in multiple concurrent partitions in parallel.',
  author='ZThor5',
  install_requires=['requests']
 )
